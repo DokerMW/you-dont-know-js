d4c9f31bf22da2d3bc230b319737314a6880eaaf
DokerMW <abddoker@gmail.com>
2024-04-04 16:43:37 +0500

    add: books ordering

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const books = document.querySelector('.books');
+const book = document.querySelectorAll('.book');
+
+books.append(book[1])
+books.append(book[0])
+books.append(book[4])
+books.append(book[3])
+books.append(book[5])
+books.append(book[2])
